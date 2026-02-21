cbe53d02322cb4cd9ecb468d855b6af49bcdf5f9
Ronald S <acyclic6@gmail.com>
2026-02-21 17:42:09 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,24 @@
+def readFiles():
+    pass
 
+def cleanParisData(data):
+    pass
+
+def addAgeColumn(data):
+    pass
+
+def createMedalTally():
+    pass
+
+def writeNewFiles():
+    pass
+
+def main():
+    readFiles()
+    cleanParisData(None)
+    addAgeColumn(None)
+    createMedalTally()
+    writeNewFiles()
+
+if __name__ == "__main__":
+    main()
